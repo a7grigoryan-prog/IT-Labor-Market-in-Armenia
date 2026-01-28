ce70180c871ab733638489e77fa42c898f1e401f
a7grigoryan-prog <a7grigoryan@gmail.com>
2026-01-28 22:28:35 +0400

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { ScrollProgress } from '@/components/ScrollProgress';
+import { HeroSection } from '@/components/HeroSection';
+import { EmploymentSection } from '@/components/EmploymentSection';
+import { SalarySection } from '@/components/SalarySection';
+import { SectorComparisonSection } from '@/components/SectorComparisonSection';
+import { InsightSection } from '@/components/InsightSection';
+import { Footer } from '@/components/Footer';
+
+const Index = () => {
+  // Enable dark mode by default
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ScrollProgress />
+      
+      <main>
+        <HeroSection />
+        <EmploymentSection />
+        <SalarySection />
+        <SectorComparisonSection />
+        <InsightSection />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
